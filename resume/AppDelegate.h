c4a141ad4e340940ad88f825ca308251268ad80d
John Black <john.f.black@icloud.com>
2016-08-07 15:13:19 -0400

    first commit for iOS version of my resume

---
+++ resume/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  resume
+//
+//  Created by John Black on 8/7/16.
+//  Copyright © 2016 John Black. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ resume/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  resume
+//
+//  Created by John Black on 8/7/16.
+//  Copyright © 2016 John Black. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
